4539b04b203bee315d2e7bc2f023c0354b6a16b0
Dauclas <shadaresearchwriters@gmail.com>
2025-03-18 20:34:44 +0300

    Add files via upload

---
+++ widget.js
@@ -0,0 +1,6 @@
+(function() {
+    var script = document.createElement('script');
+    script.src = "https://general-runtime.voiceflow.com/state/user/mem_sb_cm8ef1y42048e0wqx6lbk38gy/interact";
+    script.async = true;
+    document.body.appendChild(script);
+})();
